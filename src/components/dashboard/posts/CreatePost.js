2c5a9d3239622f590a2a04a3b7840779a944c564
Teresa Loafman <teresasanders77@gmail.com>
2021-04-19 13:59:01 -0600

    Create Post 2

--- src/components/dashboard/posts/CreatePost.js
+++ src/components/dashboard/posts/CreatePost.js
@@ -8,19 +8,63 @@ class CreatePost extends React.Component {
         this.state = {
             title: "",
             content: "",
-            userID: "",
             token: "",
             postCreated: false,
-            loading: false
+            loading: false,
+            message: ""
         };
     }
 
+    handleFormSubmit() {}
+
+    handleInputChange() {}
+
     componentDidMount() {
-        const_userID = localStorage.getItem("userID");
+        const token = localStorage.getItem("token");
+        this.setState({ token });
     }
 
     render() {
-        return <DashboardLayout>Create Post</DashboardLayout>;
+        const { loading, message, postCreated } = this.state;
+        return (
+            <DashboardLayout>
+                <form onSubmit={this.handleFormSubmit} className="mt-5" style={{ maxWidth: "800px" }}>
+                    <legend className="mb-4">Create Post</legend>
+
+                    {message ? (
+                        <div className={`alert ${postCreate ? "alert-success" : "alert-danger"}`}>{message}</div>
+                    ) : (
+                        ""
+                    )}
+                    {/* Title */}
+                    <div className="form-group">
+                        <label htmlFor="title">Title</label>
+                        <input
+                            type="text"
+                            name="title"
+                            onChange={this.handleInputChange}
+                            className="form-control"
+                            id="title"
+                        />
+                    </div>
+                    {/* Content */}
+                    <div className="form-group">
+                        <label htmlFor="my-post-content">Content</label>
+                        <textarea
+                            name="content"
+                            className="form-control"
+                            id="my-post-content"
+                            onChange={this.handleInputChange}
+                            rows="10"
+                        />
+                    </div>
+                    {/* Submit button */}
+                    <button type="submit" className="btn btn-secondary">
+                        Submit
+                    </button>
+                </form>
+            </DashboardLayout>
+        );
     }
 }
 
